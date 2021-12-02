c511562ebf992973dc84457f41a98fa105de4965
Ziggy <ziggyhand.dev@gmail.com>
2021-12-02 14:43:35 +0100

    scroll up

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,5 @@
 $(window).scroll(function () {
-  if ($(this).scrollTop() > 300) {
+  if ($(this).scrollTop() > 100) {
     $(".scrollToTop").show(300);
   } else {
     $(".scrollToTop").hide(300);
